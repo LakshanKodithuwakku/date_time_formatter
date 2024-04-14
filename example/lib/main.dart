67ea27a62350abd82f63b5735f8279d26edf00c9
Lakshan Kodithuwakku <lakshan.kodithuwakku@elegantmedia.lk>
2024-04-14 22:59:24 +0530

    version updates

---
+++ example/lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:date_time_formatter/date_time_formatter.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Date Time Formatter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Date Time Formatter Demo'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+
+          /// remove null and pass your timestamp object in here to format it
+          child: TimeFormatter(
+        /// ToDo: pass your timestamp object
+        time: null,
+      )),
+    );
+  }
+}
